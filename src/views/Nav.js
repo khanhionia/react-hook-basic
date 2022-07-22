99fd2d66c6d177ef03eb3384e382cec6216f393b
khanhionia <khanhioniakira@gmail.com>
2022-07-22 10:32:32 +0700

    video5

---
+++ src/views/Nav.js
@@ -0,0 +1,16 @@
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <div>
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
